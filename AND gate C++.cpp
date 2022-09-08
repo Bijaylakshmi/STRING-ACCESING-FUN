09e3de3d3d5b402b24853ba86a8045149136aa1e
Bijaylakshmi Kumari <112059431+Bijaylakshmi@users.noreply.github.com>
2022-09-08 14:38:50 +0530

    Add files via upload

---
+++ AND gate C++.cpp
@@ -0,0 +1,25 @@
+
+// C++ program implementing the OR gate
+// using + operator
+  
+#include <iostream>
+using namespace std;
+  
+int main()
+{
+    int a[5] = { 1, 0, 1, 0, 1 };
+    int b[5] = { 0, 1, 1, 0, 0 };
+    int i, or_ans;
+  
+    for (i = 0; i < 5; i++) {
+  
+        // using the + operator
+        if (a[i] + b[i] > 0)
+            or_ans = 1;
+        else
+            or_ans = 0;
+  
+        printf("\n %d AND %d = %d",
+               a[i], b[i], or_ans);
+    }
+}
